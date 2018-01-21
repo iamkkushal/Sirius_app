900dd9b0cea801669959b3b1e1e1f1e7a503c1de
unknown <admin@ITPRLABSYS25.velait.edu>
2018-01-21 14:15:45 +0530

    day 3

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,5 +1,14 @@
 var movies = require('./movieData');
 var dbService=require('../services/dbServices')
 exports.getAllMovies = function(req, res, next){
-  return res.json(movies);
-};
+  var db = dbService.database;
+  var moviesCollection=db.collection("movies");
+  moviesCollection.find().toArray().then(function(result){
+    var outputJSON ={
+    "isSucess":true,
+    "data":result
+  }
+  return res.json(outputJSON);
+});
+
+}
--- services/dbServices.js
+++ services/dbServices.js
@@ -2,6 +2,6 @@ var MongoClient=require('mongodb').MongoClient
 exports.createConnection=function(){
   MongoClient.connect("mongodb://iamkkushal:kushalchand@ds111138.mlab.com:11138/projector").then(function(client){
   console.log("Successfully connected with MONGODB");
-  exports.database=client.db("projector1");
+  exports.database=client.db("projector");
 });
 }
